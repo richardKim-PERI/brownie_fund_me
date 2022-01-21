e933b2e3946386304e96bb328017891120c310e2
richardKim-PERI <rickykim7979@gmail.com>
2022-01-21 17:46:27 +0900

    removed log

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -9,7 +9,6 @@ def fund():
     print(f"The current entry fee is {entrance_fee}")
     print("Funding")
     fund_me.fund({"from": account, "value": entrance_fee})
-    print("Funded!")
 
 def withdraw():
     fund_me = FundMe[-1]
